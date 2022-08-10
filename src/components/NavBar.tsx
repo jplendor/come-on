ef755d942e37cc9724457eb99b33092ea12b4845
YeaJeong <bananana0118@gmail.com>
2022-08-10 12:37:31 +0900

    🎨 Design : Navbar css수정
    + Navbar 그림자 추가, bold 설정
    
    CO-56

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,82 +1,76 @@
 import React from "react"
-import { Grid, Avatar } from "@mui/material"
-import { Map, Groups } from "@mui/icons-material"
+import { Grid, Avatar, Hidden } from "@mui/material"
+import { Map, Groups, HideImageOutlined } from "@mui/icons-material"
 import styled from "@emotion/styled"
 
-const Item = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 1rem 0;
-  &:hover {
-    color: #d90f0fde;
-    scale: (1.2);
-  }
+const CSS_ITEM = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "1rem 0",
+  margin: "0 auto",
+  transition: "0.3s",
+  "&:hover": {
+    color: "#92b4ec",
+    cursor: "pointer",
+  },
+}
+
+const MyBox = styled.div`
+  box-shadow: 0 3px 3px rgba(180, 184, 181, 0.5);
 `
+const SubTitle = styled.div`
+  font-weight: bold;
+`
+
 const Navbar: React.FC = () => {
   return (
-    <Grid
-      container
-      maxWidth="xs"
-      sx={{
-        backgroundColor: "#ffffff",
-        position: "absolute",
-        bottom: "0",
-      }}
-    >
+    <MyBox>
       <Grid
-        item
-        xs={4}
+        container
+        maxWidth="xs"
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: "1rem 0",
+          backgroundColor: "#ffffff",
+          position: "absolute",
+          bottom: "0",
+          boxShadow: " 0 -3px 3px rgba(180,184,181,.5)",
+          overflow: "hidden",
         }}
-        // onClick={}
       >
-        <Item>
-          <Map sx={{ fontSize: 40 }} />
-          <div>우리동네코스</div>
-        </Item>
+        <Grid
+          item
+          xs={4}
+          sx={CSS_ITEM}
+          // onClick={}
+        >
+          <Map sx={{ fontSize: 50 }} />
+          <SubTitle>우리동네코스</SubTitle>
+        </Grid>
+        <Grid
+          item
+          xs={4}
+          sx={CSS_ITEM}
+          // onClick={}
+        >
+          <Groups sx={{ fontSize: 50 }} />
+          <SubTitle>모임관리</SubTitle>
+        </Grid>
+        <Grid
+          item
+          xs={4}
+          sx={CSS_ITEM}
+          // onClick={}
+        >
+          <Avatar
+            alt="myImg"
+            src="/src/assets/profileImg.jpg"
+            sx={{ marginTop: 0.5, marginBottom: 0.7 }}
+          />
+          <SubTitle>마이페이지</SubTitle>
+        </Grid>
       </Grid>
-      <Grid
-        item
-        xs={4}
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: "1rem 0",
-        }}
-        // onClick={}
-      >
-        <Groups sx={{ fontSize: 40 }} />
-        모임관리
-      </Grid>
-      <Grid
-        item
-        xs={4}
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: "1rem 0",
-        }}
-        // onClick={}
-      >
-        <Avatar
-          alt="myImg"
-          src="/src/assets/profileImg.jpg"
-          sx={{ fontSize: 30 }}
-        />
-        마이페이지
-      </Grid>
-    </Grid>
+    </MyBox>
   )
 }
 
