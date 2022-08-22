5ac6e839c481b776e14ae6a65ccd31d522124343
jeongbaebang <57677452+jeongbaebang@users.noreply.github.com>
2022-08-23 02:19:42 +0900

    🖌️ Modify: 절대경로 설정
    상대 경로가 아닌 절대 경로로 작업이 가능하게 수정.
    
    .eslint ignore 파일 추가 및 절대 경로 작업에 필요한 파일 수정

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,18 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable import/no-extraneous-dependencies */
-/* config-overrides.js */
-/* eslint-disable react-hooks/rules-of-hooks */
 const { useBabelRc, override } = require("customize-cra")
 
-module.exports = override(useBabelRc())
+const path = require("path")
+
+const overrides = (config) => {
+  config.resolve = {
+    ...config.resolve,
+    alias: {
+      ...config.alias,
+
+      hooks: path.resolve(__dirname, "src/hooks"),
+      types: path.resolve(__dirname, "src/types"),
+    },
+  }
+  return config
+}
+
+module.exports = override(overrides, useBabelRc())
