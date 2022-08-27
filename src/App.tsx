00808ad8b8bb5519cd0a9e61fef003c167203095
jeongbaebang <57677452+jeongbaebang@users.noreply.github.com>
2022-08-27 23:30:08 +0900

    🖌️ Modify: 라우터 재조정 & kakao Map 임시 비활성화
    라우터 경로가 많아짐에 따라 auth/ 최초 진입점 구성
    외부 라우터 경로 비활성화 -> 개발에 불필요한 로그발생

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,26 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 
-import Course from "./pages/Course"
-
 // Redux 테스트용
-import DisplayGeolocation from "./features/geolocation/DisplayGeo"
-// 2022-08-22
-import Login from "./pages/auth/Login"
-import LoginRedirect from "./pages/auth/LoginRedirect"
+import Auth from "pages/auth/Auth"
+import DisplayGeolocation from "features/geolocation/DisplayGeo"
+
+// Auth 테스트용
+import Temporary from "pages/auth/Temporary"
 
 const App = (): JSX.Element => {
   return (
     <Routes>
-      <Route path="/" element={<DisplayGeolocation />} />
-      <Route path="auth">
-        <Route path="login" element={<Login />} />
-        <Route path="redirect" element={<LoginRedirect />} />
-      </Route>
-
+      <Route
+        path="/"
+        element={
+          <>
+            <Temporary />
+            <DisplayGeolocation />
+          </>
+        }
+      />
+      <Route path="auth/*" element={<Auth />} />
       {/* <Route path="/course/:id" element={<Course />} />
         <Route path="/course/register" />
         <Route path="/meeting" />
