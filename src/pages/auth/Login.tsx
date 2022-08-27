afe7cfc94b4332b0958c1519208ac26283a937aa
jeongbaebang <57677452+jeongbaebang@users.noreply.github.com>
2022-08-27 23:45:54 +0900

    ➕ Add: 로그인 컴포넌트
    로그인 권한이 부족하거나, 로그인이 필요할때 해당 컴포넌트가 렌더링 된다.

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,7 +1,53 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { useLocation } from "react-router-dom"
+
+import { Location } from "types/auth"
+import { conversionToUrl } from "utils"
+import useAuth from "hooks/auth/useAuth"
+import useSavePath from "hooks/auth/useSavePath"
+import useUrlRoute from "hooks/auth/useUrlRoute"
+
+import { useDidMount } from "rooks"
+import KakaoLogin from "./KakaoLogin"
+import SocialLogin from "./SocialLogin"
 
 const Login = (): JSX.Element => {
-  return <div>login</div>
+  const location = useLocation()
+  const { LoginStatus } = useAuth()
+  const { urlRoute } = useUrlRoute()
+  const { getPreviousPathName, setPreviousPathName, removePreviousPathName } =
+    useSavePath()
+
+  useDidMount(() => {
+    setPreviousPathName({
+      from: {
+        pathname: conversionToUrl(location as Location, [
+          "state",
+          "from",
+          "pathname",
+        ]),
+      },
+    })
+  })
+
+  useEffect(() => {
+    urlRoute(LoginStatus, ({ isloggedin }, go) =>
+      go(
+        isloggedin === true,
+        conversionToUrl(getPreviousPathName(), ["from", "pathname"])
+      )
+    )
+    return () => removePreviousPathName()
+  }, [urlRoute, LoginStatus, getPreviousPathName, removePreviousPathName])
+
+  return (
+    <main>
+      <h1>유저 권한이 없습니다. 로그인 하세요</h1>
+      <SocialLogin>
+        <KakaoLogin />
+      </SocialLogin>
+    </main>
+  )
 }
 
 export default Login
