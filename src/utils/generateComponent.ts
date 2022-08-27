7955e776a8e2b86e5c67f33827dd6be759954b8b
jeongbaebang <57677452+jeongbaebang@users.noreply.github.com>
2022-08-27 23:15:39 +0900

    🖌️ Modify: generateComponent 유틸함수
    주석 수정, 타입분리

--- src/utils/generateComponent.ts
+++ src/utils/generateComponent.ts
@@ -2,30 +2,24 @@
 import { v4 as uuidv4 } from "uuid"
 import { map, pipe, toArray } from "@fxts/core"
 
+type GenerateComponent = <T>(
+  datas: T[],
+  f: (data: T, key?: string) => JSX.Element
+) => JSX.Element[]
+
 /**
- * 테이터를 렌더링 할 자료구조를 넣고 렌더링 될 JSX를 넣어주면 렌더링 된 배열이 반환됩니다.
- * **내부에서 만들어진 key값을 반드시 속성으로 넘겨주어야 합니다.**
+ * 테이터를 렌더링 할 자료구조를 넣고,
+ * 
+ * 렌더링 될 JSX를 넣어주면 렌더링 된 배열이 반환됩니다.
+ * 
+ * **외부에서 전달할 key값이 없다면 내부에서 생성된 key값을 사용가능합니다.**
+ * 
+ * 내부에서 생성되는 key 값은 무작위 문자열 입니다.
  *  * createAt: 2022-08-09
  *  * author: jeongbaebang
+ * 
  * @example
  * ```tsx
- * 
-interface UserInfo {
-    name: string
-    email: string
-}
-
-const SAMPLE_DATA: UserInfo[] = [
-  {
-      name: "John Smith",
-      email: "john@smith.com",
-  },
-  {
-      name: "Jeong bae",
-      email: "Jeong bae@smith.com",
-  },
-]
-
 const App = (): JSX.Element => {
   return (
     <>
@@ -41,10 +35,7 @@ const App = (): JSX.Element => {
  * ```
  */
 
-export const generateComponent: <T>(
-  datas: T[],
-  f: (data: T, key: string) => JSX.Element
-) => JSX.Element[] = (data, f) =>
+export const generateComponent: GenerateComponent = (data, f) =>
   pipe(
     map((d, k = uuidv4()) => f(d, k), data),
     toArray
