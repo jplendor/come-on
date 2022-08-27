794205eb3df3ce2311a3cbb36c102a90ada9f149
jeongbaebang <57677452+jeongbaebang@users.noreply.github.com>
2022-08-27 23:47:48 +0900

    ➕ Add: 소셜 로그인 연동 작업 완료 & 테스트 컴포넌트 추가
    추후에 삭제예정
    
    CO-102

--- src/pages/auth/LoginRedirect.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const LoginRedirect = (): JSX.Element => {
-  return <div>LoginRedirect</div>
-}
-
-export default LoginRedirect
---
+++ src/pages/auth/Temporary.tsx
@@ -0,0 +1,41 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+import useAuth from "hooks/auth/useAuth"
+import { Button } from "@mui/material"
+
+/**
+ * Auth 테스트용 임시 컴포넌트. **추후 삭제 예정**
+ */
+
+const Temporary = (): JSX.Element => {
+  const { LoginStatus, loggedOutDispatch, loggedInDispatch } = useAuth()
+  const { isloggedin } = LoginStatus
+  return (
+    <>
+      {isloggedin ? (
+        <div>
+          <h1>로그인됨</h1>
+          <Button onClick={() => loggedOutDispatch()} variant="contained">
+            로그아웃
+          </Button>
+        </div>
+      ) : (
+        <h1>
+          <Link to="/auth/login">로그인 페이지 가기(비로그인)</Link>
+        </h1>
+      )}
+      <Link to="/auth/test-needed-token">로그인권한이 필요한 링크</Link>
+      <br />
+      <Link to="/auth/test-needed-token2">로그인권한이 필요한 링크2</Link>
+      <br />
+      <Link to="/auth/login">로그인 페이지 가기</Link> <br />
+      <Link to="/">홈</Link>
+      <button type="button" onClick={() => loggedInDispatch()}>
+        권한 얻기
+      </button>
+    </>
+  )
+}
+
+export default Temporary
