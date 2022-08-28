8a2f0e715ead703f45ed2b2075ef1258a9a6d072
jeongbaebang <57677452+jeongbaebang@users.noreply.github.com>
2022-08-29 00:25:10 +0900

    ➕ Add: 서버 API 타입 작성 & 🖌️ Modify: 타입 수정
    CO-146

--- src/types/auth/index.ts
+++ src/types/auth/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 export enum LocalstorageName {
   path = "previous-pathname",
 }
@@ -9,17 +11,29 @@ export enum Url {
 
 export enum ParamName {
   token = "token",
+  expiry = "expiry",
 }
 
 export type CheckURL = (url: string | object) => string
 
+export type Navigate = {
+  url: Url
+  option?: {
+    state?: any
+  }
+}
+
 export type UrlRoute = <T>(
   authState: T,
-  f: (
-    arg: T,
-    go: <CallBackRetrunType>(a: boolean, b: CallBackRetrunType) => void
-  ) => void
+  f: (arg: T, go: (a: boolean, b: Navigate) => void) => void
 ) => void
+
+export type GoUrl = (navigate: Navigate) => void
+
+export interface AuthParams {
+  token: string
+  expiry: string
+}
 
 export interface Location {
   state: {
