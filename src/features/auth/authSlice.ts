24fb3ca6a8ce327d4d948d3bb2e1ef551535722f
jeongbaebang <57677452+jeongbaebang@users.noreply.github.com>
2022-09-05 18:50:12 +0900

    ➕ Add: 서버 연동 작업 (RTKQ)
    1. 스토어 연결
    2. Auth 서버 연결

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,60 +1,100 @@
+import { isObject } from "@fxts/core"
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 
-import { RootState } from "store"
+import type {
+  ServerResponse,
+  ExceptionResponse,
+  TokenValidationResponse,
+} from "types/API"
 import {
+  setCookie,
   CookieName,
   removeCookie,
-  trueCallBack,
-  encryptCookie,
-  encryptedParamConversionToObj,
+  pullingProperty,
+  encryptedTextConvToParamObj,
 } from "utils"
+import { RootState } from "store"
+import { api } from "features/api/apiSlice"
 
-export interface AuthSliceState {
+interface AuthSliceState {
+  userId: string
   isloggedin: boolean
   status: "idle" | "pending" | "succeeded" | "failed"
 }
 
-const initialState: AuthSliceState = {
-  status: "idle",
-  isloggedin: false,
+interface ReturnType {
+  userId: string
+  encryptedText: string
 }
 
+const initialState: AuthSliceState = {
+  userId: "",
+  isloggedin: false,
+  status: "idle",
+}
+
+export const authApiSlice = api.injectEndpoints({
+  endpoints: (builder) => ({
+    validation: builder.query<ServerResponse, string>({
+      query: (token) => ({
+        url: "/auth/validate",
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
+    }),
+    logout: builder.query<ServerResponse, void>({
+      query: () => ({
+        url: "/auth/logout",
+        method: "POST",
+      }),
+    }),
+    reissue: builder.query<ServerResponse, void>({
+      query: () => ({
+        url: "/auth/reissue",
+        method: "POST",
+      }),
+    }),
+  }),
+})
+
 export const tokenValidation = createAsyncThunk<
-  string,
+  ReturnType,
   string,
   { state: RootState }
 >(
   "auth/tokenValidation",
 
   /**
-   * authData 매개변수에 두가지 경우가 들어온다.
-   *  1. 로그인API파라미터로 받아오는 token.
-   *  2. 저장된 토큰쿠키를 조회하여 받아오는 token.
+   * encryptedText 매개변수에 두가지 경우가 들어온다.
+   *  1. 로그인API파라미터로 받아오는 encryptedText.
+   *  2. 저장된 토큰쿠키를 조회하여 받아오는 encryptedText.
    * 서버에는 항상 올바른 토큰만을 전송하게끔 해야한다.
    * 토큰 만료 등 정상적인 현상은 상관없다.
    *
-   * 토큰 쿠키를 복호화 시키면 token, expiry 값이 들어있다.
+   * 토큰 쿠키를 복호화 시키면 token, expiry, userId 값이 들어있다.
    */
 
   // FINISH: 서버API타입정의하기 [v]
   // FINISH: 요청 중복 방지 처리 [v]
   // FINISH: token 값이 없다면 에러 디스패치 [v]
   // 에러 디스패치 조건 -> 암호화된 쿠키값이 변조되었을때.
-  // TODO: 토큰 만료 처리 [] -> 만료되면 서버로 보내지 말기
-  //  1.TODO: 토큰이 만료되었다면 토큰 재발급 API 전송 []
-  //  2.TODO: 토큰 만료 비교 함수 만들기 []
 
-  async (authData: string, { rejectWithValue }) => {
-    trueCallBack(!authData, () =>
-      rejectWithValue("암호화 쿠키가 존재하지 않거나 변조됨.")
+  async (encryptedText: string, { rejectWithValue, dispatch }) => {
+    const { token } = encryptedTextConvToParamObj(encryptedText)
+    const { data, isError } = await dispatch(
+      authApiSlice.endpoints.validation.initiate(token)
     )
-
-    return (await new Promise((resolve) => {
-      // 해당 로직에서 서버로 토큰 전송
-      setTimeout(() => {
-        resolve(authData)
-      }, 500)
-    })) as string
+    if (isError) return rejectWithValue(data as ExceptionResponse)
+    const userId = pullingProperty(data as TokenValidationResponse, [
+      "data",
+      "userId",
+    ])
+    return {
+      encryptedText,
+      userId: isObject(userId) ? "null" : userId,
+    }
   },
   {
     condition: (_, { getState }) => {
@@ -74,36 +114,36 @@ const authSlice = createSlice({
     loggedIn: (state) => {
       state.isloggedin = true
     },
-    loggedOut: (state) => {
-      removeCookie(CookieName.auth)
-      state.status = "idle"
-      state.isloggedin = false
-    },
   },
   extraReducers: (builder) => {
     builder.addCase(tokenValidation.pending, (state) => {
-      const paramObj = encryptedParamConversionToObj()
-      console.log("복호화된 객체:", paramObj)
       state.status = "pending"
     })
-    /**
-     * 유효한 토큰값이 들어올것을 기대한다.
-     * 해당 토큰값을 암호화 시킨다.
-     */
-    builder.addCase(tokenValidation.fulfilled, (state, action) => {
-      encryptCookie(CookieName.auth, action.payload)
-      state.status = "succeeded"
-      state.isloggedin = true
-    })
+    builder.addCase(
+      tokenValidation.fulfilled,
+      (state, { payload: { encryptedText, userId } }) => {
+        setCookie(CookieName.auth, encryptedText)
+        state.userId = userId
+        state.isloggedin = true
+        state.status = "succeeded"
+      }
+    )
     builder.addCase(tokenValidation.rejected, (state) => {
       removeCookie(CookieName.auth)
-      state.status = "failed"
       state.isloggedin = false
+      state.status = "failed"
     })
+    builder.addMatcher(
+      authApiSlice.endpoints.logout.matchFulfilled,
+      (state) => {
+        removeCookie(CookieName.auth)
+        state.isloggedin = false
+      }
+    )
   },
 })
 
-export const { loggedIn, loggedOut } = authSlice.actions
+export const { loggedIn } = authSlice.actions
 
 export const stateAuth = (state: RootState): AuthSliceState => state.auth
 
--- src/pages/auth/KakaoLogin.tsx
+++ src/pages/auth/KakaoLogin.tsx
@@ -1,23 +1,22 @@
 import React, { useState } from "react"
 
+import { Login } from "types/API"
 import RedirectURL from "./RedirectURL"
 
 /**
  * 서버에 kakao API를 보내기 위해 특정 URL로 전송시키고 token을 받아온다.
- * 받은 토큰은 /auth/login-api 링크로 전송된다.
+ * 받은 토큰은 /auth/login-verification 링크로 전송된다.
  */
+
+const { KAKAO, REDIRECT_URI } = Login
+const CLIENT = process.env.REACT_APP_CLIENT_URL
+const SERVER = process.env.REACT_APP_SERVER_URL
 
 const KakaoLogin = (): JSX.Element => {
   const [isClick, setIsClick] = useState(false)
   const onClickHandler = (): void => setIsClick(true)
-
-  if (isClick)
-    return (
-      // 추후에 서버에서 제공하는 로그인 API 경로를 전달한다.
-      // eslint-disable-next-line max-len
-      <RedirectURL url="http://localhost:3000/auth/login-verification?token=0xe10742928d01ac23eab87a6c6e2ef90&expiry=1661699810740" />
-    )
-
+  const KAKAO_LOGIN = `${SERVER}/oauth2/authorize/${KAKAO}?redirect_uri=${CLIENT}${REDIRECT_URI}`
+  if (isClick) return <RedirectURL url={KAKAO_LOGIN} />
   return (
     <>
       <div>카카오 로그인 페이지</div>
