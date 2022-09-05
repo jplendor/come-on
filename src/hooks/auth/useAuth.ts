d8f3bcc951f5e42abba11a4e7ff620cb23541057
jeongbaebang <57677452+jeongbaebang@users.noreply.github.com>
2022-09-05 18:51:39 +0900

    🖌️ Modify: 매개변수명 변경& 일부 로직 수정
    액션 디스패치 할때 넘길 인자값을 암호화 해서 전달.

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import { CookieName, decryptCookie } from "utils"
+import { CookieName, getCookie } from "utils"
 import {
   loggedIn,
-  loggedOut,
   stateAuth,
+  authApiSlice,
   tokenValidation,
 } from "features/auth/authSlice"
 import { useAppDispatch, useAppSelector } from "hooks/redux/useRedux"
@@ -12,19 +12,20 @@ const useAuth = () => {
   const dispatch = useAppDispatch()
   const LoginStatus = useAppSelector(stateAuth)
   const loggedInDispatch = () => dispatch(loggedIn())
-  const loggedOutDispatch = () => dispatch(loggedOut())
+  const loggedOutDispatch = () =>
+    dispatch(authApiSlice.endpoints.logout.initiate(undefined))
 
   /**
-   * @param token 검증할 토큰값을 인자로 넣어줍니다.
+   * @param encryptedText 암호화된 텍스트를 전달합니다.
    * 인자를 넘기지 않는다면 ENCRYPT_TOKEN 쿠키를 조회하여 값을 할당합니다.
    */
 
-  // 토큰을 전달하지 않는 경우가 있는가? -> Yes
-  // 어떤경우에? -> 토큰쿠키를 갖고있다고 판단될때
+  // 암호화된 텍스트를 전달하지 않는 경우가 있는가? -> Yes
+  // 어떤경우에? -> 암호화쿠키를 갖고있다고 판단될때
 
   const dispatchTokenValidation = (
-    token: string = decryptCookie(CookieName.auth)
-  ) => dispatch(tokenValidation(token))
+    encryptedText: string = getCookie(CookieName.auth)
+  ) => dispatch(tokenValidation(encryptedText))
 
   return {
     LoginStatus,
