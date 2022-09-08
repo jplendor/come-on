bff3038affc0025d730954b14b3d24ef5aac50a9
jeongbaebang <57677452+jeongbaebang@users.noreply.github.com>
2022-09-08 12:48:10 +0900

    ➕ Add: 새로운 절대 경로 추가
    1. assets
    2. components
    3. theme

--- config-overrides.js
+++ config-overrides.js
@@ -7,12 +7,15 @@ const overrides = (config) => {
     ...config.resolve,
     alias: {
       ...config.alias,
+      assets: path.resolve(__dirname, "src/assets"),
+      components:path.resolve(__dirname, "src/components"),
       store: path.resolve(__dirname, "src/app/store.ts"),
       features: path.resolve(__dirname, "src/features"),
       hooks: path.resolve(__dirname, "src/hooks"),
       pages: path.resolve(__dirname, "src/pages"),
       types: path.resolve(__dirname, "src/types"),
       utils: path.resolve(__dirname, "src/utils/index.ts"),
+      theme: path.resolve(__dirname, "src/theme.ts"),
     },
   }
   return config
