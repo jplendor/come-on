8048c4c76c964800b8c5251d20b9fd9dfaaa5647
jeongbaebang <57677452+jeongbaebang@users.noreply.github.com>
2022-09-15 20:46:55 +0900

    ➕ Add: 테스트용 컴포넌트 추가
    사진 업로드 로직 임시 발행 -> 유저프로필 API 테스트를 위함

--- src/pages/auth/Temporary.tsx
+++ src/pages/auth/Temporary.tsx
@@ -1,6 +1,5 @@
 import React from "react"
-import { Button } from "@mui/material"
-import { Link } from "react-router-dom"
+import { Button, Link } from "@mui/material"
 
 import useAuth from "hooks/auth/useAuth"
 
@@ -13,27 +12,27 @@ const Temporary = (): JSX.Element => {
   const { isloggedin } = LoginStatus
   return (
     <>
-      <Link to="/auth/test-needed-token">로그인권한이 필요한 링크</Link>
+      <Link href="/auth/test-needed-token">로그인권한이 필요한 링크</Link>
       <br />
-      <Link to="/auth/test-needed-token2">로그인권한이 필요한 링크2</Link>
+      <Link href="/auth/test-needed-token2">로그인권한이 필요한 링크2</Link>
       <br />
-      <Link to="/user/my-page">마이페이지 가기</Link>
+      <Link href="/user/my-page">마이페이지 가기</Link>
       <br />
-      <Link to="/auth/login">로그인 페이지 가기</Link> <br />
-      <Link to="/">홈</Link>
+      <Link href="/auth/login">로그인 페이지 가기</Link> <br />
+      <Link href="/">홈</Link>
       <button type="button" onClick={() => loggedInDispatch()}>
         권한 얻기
       </button>
       {isloggedin ? (
         <div>
           <p>로그인됨</p>
-          <Button onClick={() => loggedOutDispatch()} variant="contained">
+          <Button onClick={loggedOutDispatch} variant="contained">
             로그아웃
           </Button>
         </div>
       ) : (
         <p>
-          <Link to="/auth/login">로그인 페이지 가기(비로그인)</Link>
+          <Link href="/auth/login">로그인 페이지 가기(비로그인)</Link>
         </p>
       )}
     </>
