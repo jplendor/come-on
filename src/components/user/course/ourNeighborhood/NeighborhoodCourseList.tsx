90970c9d4eabe46d6d6f3392c85c9a5a5c4d589f
jeongbaebang <57677452+jeongbaebang@users.noreply.github.com>
2022-09-16 21:05:27 +0900

    ➕ Add: 우리동네코스 리스트 컴포넌트 추가

---
+++ src/components/user/course/ourNeighborhood/NeighborhoodCourseList.tsx
@@ -0,0 +1,52 @@
+import React from "react"
+import { Grid, Stack } from "@mui/material"
+
+import CardItem from "components/common/card/CardItem"
+
+interface NeighborhoodCourseProps {
+  info: {
+    img: {
+      src: string
+      alt: string
+    }
+    isLike: boolean
+    texts: {
+      title: string
+      userName: string
+      time: string
+    }
+  }
+}
+const NeighborhoodCourseList = ({
+  info,
+}: NeighborhoodCourseProps): JSX.Element => {
+  return (
+    <Grid
+      item
+      container
+      wrap="nowrap"
+      direction="column"
+      component="section"
+      sx={{
+        maxHeight: "585px",
+        overflowY: "auto",
+        overflowX: "hidden",
+      }}
+    >
+      <Stack
+        spacing={2}
+        sx={{
+          padding: "21px",
+        }}
+      >
+        <CardItem info={info} />
+        <CardItem info={info} />
+        <CardItem info={info} />
+        <CardItem info={info} />
+        <CardItem info={info} />
+      </Stack>
+    </Grid>
+  )
+}
+
+export default NeighborhoodCourseList
