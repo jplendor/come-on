d8db415f8d74dabc9a957c1ed7f752f512161d7a
jeongbaebang <57677452+jeongbaebang@users.noreply.github.com>
2022-09-18 00:20:51 +0900

    ➕ Add: 유저 API slice 추가 (닉네임 수정 API)

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import { api } from "features/api/apiSlice"
-import { MydetailResponse } from "types/API/user-service"
+import { ModifyNameResponse, MydetailResponse } from "types/API/user-service"
 
 export const userApiSlice = api.injectEndpoints({
   endpoints: (builder) => ({
@@ -11,10 +11,21 @@ export const userApiSlice = api.injectEndpoints({
       }),
       providesTags: ["User"],
     }),
+    modifyName: builder.mutation<ModifyNameResponse, string>({
+      query: (name) => ({
+        url: "/users/me",
+        method: "PATCH",
+        body: {
+          nickname: name,
+        },
+      }),
+      invalidatesTags: ["User"],
+    }),
   }),
 })
 
 export const {
   useMyDetialQuery,
+  useModifyNameMutation,
   endpoints: { myDetial },
 } = userApiSlice
