6987bde2c6305c2dbd199ff9f23afc375a15af33
jeongbaebang <57677452+jeongbaebang@users.noreply.github.com>
2022-09-18 00:14:33 +0900

    🖌️ Modify: 반환 형태 간소화

--- src/components/user/profile/ProfileSkeleton.tsx
+++ src/components/user/profile/ProfileSkeleton.tsx
@@ -1,69 +1,63 @@
 import React from "react"
 import { Grid, Skeleton, Stack } from "@mui/material"
 
-const ProfileInfoSkeleton = (): JSX.Element => {
-  return (
-    <Stack
-      component="article"
-      alignItems="center"
-      justifyContent="center"
+const ProfileInfoSkeleton = (): JSX.Element => (
+  <Stack
+    component="article"
+    alignItems="center"
+    justifyContent="center"
+    sx={{
+      height: "125px",
+    }}
+    spacing={1}
+  >
+    <Grid justifyContent="center">
+      <Skeleton variant="circular" width={70} height={70} />
+    </Grid>
+    <Skeleton
+      width={140}
+      height={25}
       sx={{
-        height: "125px",
+        paddingTop: "8px",
       }}
-      spacing={1}
-    >
-      <Grid justifyContent="center">
-        <Skeleton variant="circular" width={70} height={70} />
-      </Grid>
-      <Skeleton
-        width={140}
-        height={25}
-        sx={{
-          paddingTop: "8px",
-        }}
-      />
-      <Skeleton width={95} height={20} />
-    </Stack>
-  )
-}
+    />
+    <Skeleton width={95} height={20} />
+  </Stack>
+)
 
-const ProfileNicknameSkeleton = (): JSX.Element => {
-  return (
-    <Stack
-      component="article"
+const ProfileNicknameSkeleton = (): JSX.Element => (
+  <Stack
+    component="article"
+    sx={{
+      mt: "16px",
+      px: "20px",
+    }}
+  >
+    <Grid container pb={1}>
+      <Grid item xs>
+        <Skeleton width={73} height={18} />
+      </Grid>
+      <Grid item xs={7} />
+      <Grid item xs>
+        <Skeleton width={73} height={18} />
+      </Grid>
+    </Grid>
+    <Skeleton
+      variant="rounded"
+      height={27}
       sx={{
-        mt: "16px",
-        px: "20px",
+        gap: "10px",
+        padding: "12px",
       }}
-    >
-      <Grid container pb={1}>
-        <Grid item xs>
-          <Skeleton width={73} height={18} />
-        </Grid>
-        <Grid item xs={7} />
-        <Grid item xs>
-          <Skeleton width={73} height={18} />
-        </Grid>
-      </Grid>
-      <Skeleton
-        variant="rounded"
-        height={27}
-        sx={{
-          gap: "10px",
-          padding: "12px",
-        }}
-      />
-    </Stack>
-  )
-}
+    />
+  </Stack>
+)
 
-const ProfileSkeleton = (): JSX.Element => {
-  return (
-    <>
-      <ProfileInfoSkeleton />
-      <ProfileNicknameSkeleton />
-    </>
-  )
-}
+const ProfileSkeleton = (): JSX.Element => (
+  <>
+    <ProfileInfoSkeleton />
+    <ProfileNicknameSkeleton />
+  </>
+)
 
 export default ProfileSkeleton
