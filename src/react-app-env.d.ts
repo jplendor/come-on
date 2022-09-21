32feacedfc8a9a8526d921293cf9668f6c4c8621
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-09-21 19:10:53 +0900

    ➕ Add: 테스트 배포 작업 추가

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -9,5 +9,13 @@ declare namespace NodeJS {
     REACT_APP_SECRET_KEY: string
     REACT_APP_CLIENT_URL: string
     REACT_APP_SERVER_URL: string
+
+    REACT_APP_APIKEY: string
+    REACT_APP_AUTHDOMAIN: string
+    REACT_APP_PROJECTID: string
+    REACT_APP_STORAGEBUCKET: string
+    REACT_APP_MESSAGINGSENDERID: string
+    REACT_APP_APPID: string
+    REACT_APP_MEASUREMENTID: string
   }
 }
