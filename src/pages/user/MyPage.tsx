c935f3998bcd90ba92e2ff0901668dc6d451a3e0
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-09-28 13:53:23 +0900

    🔨 Refactor: 마이페이지 컴포넌트 간소화

--- src/pages/user/MyPage.tsx
+++ src/pages/user/MyPage.tsx
@@ -1,30 +1,14 @@
+/* eslint-disable no-lone-blocks */
+/* eslint-disable prettier/prettier */
 import React, { useEffect } from "react"
-import { Box, Grid } from "@mui/material"
 
-import UserCourse from "components/user/course/UserCourse"
-import UserProfile from "components/user/profile/UserProfile"
 import useNavigate from "hooks/navigate/useNavigate"
+import CardLayout from "components/common/card/cardLayout"
 
 const MyPage = (): JSX.Element => {
   const { setIndexDispatch } = useNavigate()
   useEffect(() => setIndexDispatch(2))
-  return (
-    // 콘텐츠 영역 #1
-    <Grid container component="main" direction="column" minHeight={1}>
-      {/* 프로필 영역 #1 */}
-      <UserProfile />
-      {/* 경계선 */}
-      <Box
-        sx={{
-          mt: "25px",
-          height: "12px",
-          backgroundColor: "#F5F5F5",
-        }}
-      />
-      {/* 코스 영역 #1 */}
-      <UserCourse />
-    </Grid>
-  )
+  return <CardLayout type="MyPage" />
 }
 
 export default MyPage
