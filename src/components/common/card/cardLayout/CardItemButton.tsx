dc46eb03978b2e5c7c78d9220ec25b2c3571320d
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-09-28 14:18:56 +0900

    🔨 Refactor: CardItemButton컴포넌트 재작성

---
+++ src/components/common/card/cardLayout/CardItemButton.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import React from "react"
+
+import theme from "theme"
+import { IconButton } from "@mui/material"
+import { useClickLikeCourseMutation } from "features/course/courseSlice"
+import { FavoriteRounded, FavoriteBorderRounded } from "@mui/icons-material"
+
+interface LikeButtonProps {
+  isLike: boolean
+  courseId: number
+}
+
+export const LikeButton = ({
+  isLike,
+  courseId,
+}: LikeButtonProps): JSX.Element => {
+  const [click] = useClickLikeCourseMutation()
+  const onClickHandler = () => click(courseId)
+  return (
+    <IconButton
+      aria-label="like"
+      onClick={onClickHandler}
+      sx={{
+        width: "24px",
+        height: "24px",
+        marginTop: "12px",
+        marginRight: "14px",
+      }}
+    >
+      {isLike ? (
+        <FavoriteRounded color="secondary" />
+      ) : (
+        <FavoriteBorderRounded
+          sx={{ color: theme.palette.primary.contrastText }}
+        />
+      )}
+    </IconButton>
+  )
+}
+
+export default LikeButton
