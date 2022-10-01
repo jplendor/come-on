7f8c4537fa6862f0f49bfd4681e41e1e38289d26
jplendor <jplendor@gmail.com>
2022-10-01 14:59:38 +0900

    🎨 Design : 모임 수정 페이지 디자인 수정
    CO-251

--- src/pages/meeting/MeetingEdit.tsx
+++ src/pages/meeting/MeetingEdit.tsx
@@ -19,6 +19,7 @@ import DisplayListDetailCard, {
 } from "components/common/card/DisplayListDetailCard"
 import { MapOutlined } from "@mui/icons-material"
 import Header from "components/meeting/Header"
+import styled from "@emotion/styled"
 
 const NewPlace = {
   border: "dashed 2px gray",
@@ -30,6 +31,11 @@ const NewPlace = {
   flexDirection: "column",
   cursor: "pointer",
 }
+
+const Title = styled(Typography)`
+  font-weight: bold;
+  margin-right: 10px;
+`
 
 const MeetingEdit = (): JSX.Element => {
   const { meetingId } = useParams()
@@ -53,23 +59,16 @@ const MeetingEdit = (): JSX.Element => {
     content = <CircularProgress />
   } else if (isSuccess) {
     const { data: meeting } = response
+
     content = (
       <Container>
         <Header text={meeting.title} />
-        <Grid container spacing={3}>
-          <Grid item xs={4}>
-            <Typography variant="h5" component="h2">
-              모임 멤버
-            </Typography>
-          </Grid>
-          <Grid item xs={8}>
-            <Typography
-              variant="h5"
-              component="h2"
-              sx={{ color: theme.palette.primary.main }}
-            >
+        <Grid container spacing={3} sx={{ mt: "20px", mb: "20px" }}>
+          <Grid item xs={12} sx={{ display: "flex" }}>
+            <Title>모임멤버</Title>
+            <Title sx={{ color: theme.palette.primary.main }}>
               {meeting.meetingUsers.length}
-            </Typography>
+            </Title>
           </Grid>
           <Grid item xs={12}>
             <Stack direction="row" spacing={2}>
@@ -83,9 +82,9 @@ const MeetingEdit = (): JSX.Element => {
             </Stack>
           </Grid>
           <Grid item xs={12}>
-            <Typography variant="h5" component="h2">
-              모임 일정
-            </Typography>
+            <Title>모임일정</Title>
+          </Grid>
+          <Grid item xs={12}>
             <Calendar
               meetingInfo={{
                 meetingId: meeting.id,
@@ -97,9 +96,9 @@ const MeetingEdit = (): JSX.Element => {
             />
           </Grid>
           <Grid item xs={12}>
-            <Typography variant="h5" component="h2">
-              모임 장소
-            </Typography>
+            <Title>모임장소</Title>
+          </Grid>
+          <Grid item xs={12}>
             <div>
               {generateComponent(meeting.meetingPlaces, (data, key) => (
                 <DisplayListDetailCard
@@ -116,14 +115,16 @@ const MeetingEdit = (): JSX.Element => {
                   }}
                 />
               ))}
-              <Box sx={NewPlace} onClick={addNewPlace}>
-                <MapOutlined />
-                <Typography>새로운 장소를 추가해보세요</Typography>
-              </Box>
-              <Button variant="contained" fullWidth>
-                코스로 공유하기
-              </Button>
             </div>
+            <Box sx={NewPlace} onClick={addNewPlace}>
+              <MapOutlined />
+              <Typography>새로운 장소를 추가해보세요</Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" fullWidth>
+              코스로 공유하기
+            </Button>
           </Grid>
         </Grid>
       </Container>
