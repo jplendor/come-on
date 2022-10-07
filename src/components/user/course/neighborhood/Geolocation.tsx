8044a650b76785be850b67e40ea5d18fa3f64d89
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-10-07 15:43:18 +0900

    🔨 Refactor: geolocation 일부 로직 수정

--- src/components/user/course/neighborhood/Geolocation.tsx
+++ src/components/user/course/neighborhood/Geolocation.tsx
@@ -1,9 +1,8 @@
-import React, { useEffect } from "react"
+import React from "react"
 import {
   PlaceOutlined as PlaceOutlinedIcon,
   WrongLocation as WrongLocationIcon,
 } from "@mui/icons-material"
-import { useGeolocation as rooksGetGeo } from "rooks"
 import { Grid, IconButton, Tooltip } from "@mui/material"
 
 import useGeolocation from "hooks/geolocation/useGeolocation"
@@ -11,20 +10,10 @@ import useGeolocation from "hooks/geolocation/useGeolocation"
 const Geolocation = (): JSX.Element => {
   const {
     geoState: {
-      isDone,
       info: { isError, message },
     },
     geoManual,
-    geoUpdateDispatch,
   } = useGeolocation()
-  const geolocation = rooksGetGeo({
-    when: !isDone,
-    enableHighAccuracy: true,
-  })
-  useEffect(
-    () => geoUpdateDispatch(geolocation),
-    [geolocation, geoUpdateDispatch]
-  )
 
   return (
     <Grid container justifyContent="center" alignItems="center">
